721148e55096502cd1cd4b74f5ea55311895ce4d
radiocontrolled <radiocontrolled@gmail.com>
2015-08-25 20:55:33 +0100

    adding d3 code to make the bar chart

--- js/main.js
+++ js/main.js
@@ -1,75 +1,81 @@
 (function () {
 
- //  var button = document.getElementsByTagName("input")[2];
- //  var key, value = 0;
-
-	// // process user input
-	// var processUserInput = function() {
- //    var radioButtons = document.getElementsByName("author");
- //    for (var i = 0; i < radioButtons.length; i++ ) {
- //      if (radioButtons[i].checked === true) {
- //        key = radioButtons[i].id;
- //        value++;
- //        console.log(key);
- //        console.log(value);
-       
- //      }
- //    }
- //  };
-
- //  // add click event listener
- //  button.addEventListener("click", function (e) {
- //    e.preventDefault();
- //    processUserInput();
- //  });
-
- //  // add keydown listener for enter or spacebar
- //  button.addEventListener("keydown", function (e) {
- //    e = e || window.event;
- //    var key = e.which || e.keyCode;
- //    if ((key == 13) || (key ==32)) { 
- //        e.preventDefault();
- //        processUserInput();
- //    }
- //  });
-
   // visualisation 
 
   var results = document.getElementById("results"),
+  width =  results.offsetWidth, 
+  height = width/2, 
+  largest = 0, 
+  padding = 2, 
   data, 
-  width =  results.offsetWidth;
+  arr; 
 
   var visData = [{name: "Susan", count: 0},{name: "Camile", count: 0}];
   
-  d3.json("data.json", function(error, json) {
+  d3.json("testData.json", function(error, json) {
     if (error) {
-      return console.warn(error);
+      console.log(error);
     }
     data = json;
-    drawChart();
-
-     for(var key in data){
-      if(data[key]["name"] === "Susan") {
-        visData[0]["count"] += 1;
-      }
-      if(data[key]["name"] === "Camile") {
-        visData[1]["count"] += 1;
-      }
+ 
+    arr = [];
+ 
+    // get the largest score 
+    // for the scale domain 
+    for(var key in data){
+      if(data.hasOwnProperty(key)){
+        if(data[key].value >= largest) {
+          largest = data[key].value;      
+        }
+      }  
+      arr.push(data[key].value);
     }
-   
+
+    drawChart();
+  
   });
 
 
 
   var drawChart = function() {
-    var svg; 
+    var svg, scale; 
     svg = d3.select(results)
       .append("svg")
       .attr({
         "width": width,
         "height": width
       });
+
+    scale = d3.scale.linear()
+      .domain([0, largest])
+      .range([0, height]);
+
+    svg.selectAll("rect")
+      .data(arr)
+      .enter()
+      .append("rect")
+      .attr({
+        "width": width/arr.length - padding,
+        "height": function (d) {
+          return scale(d); 
+        }, 
+        "x" : function (d,i) {
+          return i * (width / arr.length);
+        }, 
+        "y" : function (d) {
+          return  height - scale(d); 
+        }, 
+        "fill" :  function (d) {
+          return "rgb(243, 156, " + (d * 6) + ")";
+        }
+      });
+  };
+
+  var resize = function() {
+    // resize stuff
   }
+
+  window.onresize = resize; 
 
  
 
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
-
+* to-do 
+** add labels for the chart 
+** make the chart responsive
+** wire in the JSON results from the server
